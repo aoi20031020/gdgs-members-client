5f652a27fa14757e02a54820d319312d53d7e2af
ShinichiroKan <a24.eb8g@g.chuo-u.ac.jp>
2024-06-10 12:09:46 +0900

    1st commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,47 @@
+import { background } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const AppHeader = styled.header`
   display: flex;
+  background-color: #696969;
   flex-direction: row; // Change this line
   align-items: center;
-  justify-content: center;
+  justify-content: left;
   font-size: calc(10px + 2vmin);
 `;
 
 const StyledButton = styled.button`
+  color: white;
+  background-color: #696969;
+  padding: 20px;
   margin: 0 10px; // Add this line
+  font-size: 20px;
+
 `;
 const HeaderStyle = styled.div`
-fontsize: 100px;`
+color: white;
+background-color: #696969;
+text-align: left;
+padding: 20px;
+padding-left: 20px;
+padding-right: 450px;
+font-size: 24px; //見出しのサイズ
+margin: 0px;`
 function Header() {
   return (
     <AppHeader>
+      <img src="img/GDSC.png" alt="GDSC"></img>
+      <HeaderStyle>GDSC　メンバー管理サイト</HeaderStyle>
       <Link to="/">
-        <StyledButton>Home</StyledButton>
+        <StyledButton>HOME</StyledButton>
       </Link>
       <Link to="/register">
-        <StyledButton>登録</StyledButton>
+        <StyledButton>Regester</StyledButton>
       </Link>
       <Link to="/members">
-        <StyledButton>一覧</StyledButton>
+        <StyledButton>Members</StyledButton>
       </Link>
     </AppHeader>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,65 @@
 import React from "react";
 import Header from "../components/Header";
 import { styled } from "styled-components";
+import { Link } from "react-router-dom";
 
-const HeaderStyle = styled.div`
-font-size: 100px;`
+const BodyStyle = styled.div`
+display: inline-flex;
+clip-path: polygon(0 0px, 760px 0, 500px 900px, 0 100%);
+line-height: 100%;
+color: white;
+background-color: #c0c0c0;
+padding: 120px;
+padding-left: 30px;
+padding-right: 400px;
+padding-bottom: 40px;
+font-size: 80px;`
+
+const TextStyle = styled.div`
+display: inline-block;
+color: white;
+clip-path: polygon(0 0px, 668px 0, 500px 550px, 0 100%);
+background-color: #c0c0c0;
+padding: 0px;
+padding-left: 30px;
+padding-right: 520px;
+padding-bottom: 200px;
+font-size: 20px;`
+
+const Icon1Style = styled.div`
+display: inline-flex;
+text-align: right;
+background-color: white;
+padding: 30px;
+padding-left: 30px;
+padding-right: 100px;
+padding-botton: 0px;
+font-size: 20px;`
+
+const Icon2Style = styled.div`
+display: inline-flex;
+text-align: right;
+background-color: white;
+padding: 30px;
+padding-left: 30px;
+padding-right: 0px;
+padding-botton: 100px;
+font-size: 20px;`
+
 function Home() {
   return (
-    <header>
     <div>
-      <h1>GDSC　メンバー管理サイト</h1>
-      <p>This is the home page.</p>
-      <HeaderStyle> aaaaaa</HeaderStyle>
+      <BodyStyle>GDSC<br />membership</BodyStyle>
+      <Link to="/member1">
+       <Icon1Style><img src="src/components/img/one.png" alt="one"></img>メンバー登録</Icon1Style>
+      </Link>
+      <TextStyle>説明テキストテキストテキストテキ<br />ストテキストテキストテキストテキ<br />スト</TextStyle>
+      <Link to="/member2">
+      <Icon2Style><img src="src/components/img/three.png" alt="three"></img>メンバー一覧</Icon2Style>
+      </Link>
     </div>
-    </header>
-    
     );
   }
+
 
 export default Home;
