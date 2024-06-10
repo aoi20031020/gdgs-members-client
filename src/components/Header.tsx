3c8625be9e9ab25f1d499f9eac4cc6b34d0e01d9
ShinichiroKan <a24.eb8g@g.chuo-u.ac.jp>
2024-06-10 15:07:50 +0900

    Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,14 +25,20 @@ color: white;
 background-color: #696969;
 text-align: left;
 padding: 20px;
-padding-left: 20px;
+padding-left: 0px;
 padding-right: 450px;
 font-size: 24px; //見出しのサイズ
 margin: 0px;`
+
+const Picture = styled.picture`
+padding-left: 10px;
+width: 80px;
+heights: 50px;`
+
 function Header() {
   return (
     <AppHeader>
-      <img src="img/GDSC.png" alt="GDSC"></img>
+      <Picture><img src="https://s3-alpha-sig.figma.com/img/e3c8/ab3d/0f0b4d9a35c599088de6b71baee5120e?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W7h7-aYIC59BfOvlIrGJlQe605LUY9y1O0mZY-qIyfpqeTAcKGVgmvxggUAZrNgh2ToS2jf6NdrFPR-8Tn8BAG7y2FD3RIYO7wjpIDo6YuUkw6hqb39vbXGQ6q0DNSONAunyDZL1zw7H5c5A-np7DGHHfxTQRFZ3kDSQ2NlpZu69VtkG~R3c2Cc0opMeR8dvOW6~xH-NZfWH9uA-LRi3hKjj~9p5tTPewnhDoPihO06iXgTjCn0ClWH9-wu0foFTY23dWSSH-Dg2QqqSPWdS~aqLqO37jhsh8H3rZpbj1YDSoNr4oGFUyyfVe5FDd~akS8NEzYSR~54aUxHPNaXVPw__"></img></Picture>
       <HeaderStyle>GDSC　メンバー管理サイト</HeaderStyle>
       <Link to="/">
         <StyledButton>HOME</StyledButton>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,58 +4,88 @@ import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 
 const BodyStyle = styled.div`
-display: inline-flex;
-clip-path: polygon(0 0px, 760px 0, 500px 900px, 0 100%);
+display: inline-block;
+clip-path: polygon(0 0px, 760px 0, 500px 1000px, 0 100%);
 line-height: 100%;
 color: white;
 background-color: #c0c0c0;
-padding: 120px;
+padding: 150px;
 padding-left: 30px;
-padding-right: 400px;
-padding-bottom: 40px;
+padding-right: 300px;
+padding-bottom: 10px;
 font-size: 80px;`
 
 const TextStyle = styled.div`
 display: inline-block;
 color: white;
-clip-path: polygon(0 0px, 668px 0, 500px 550px, 0 100%);
+clip-path: polygon(0 0px, 677px 0, 500px 610px, 0 100%);
 background-color: #c0c0c0;
-padding: 0px;
+padding-top: 50px;
 padding-left: 30px;
-padding-right: 520px;
-padding-bottom: 200px;
+padding-right: 420px;
+padding-bottom: 170px;
 font-size: 20px;`
 
 const Icon1Style = styled.div`
 display: inline-flex;
-text-align: right;
 background-color: white;
-padding: 30px;
+padding-top: 230px;
 padding-left: 30px;
 padding-right: 100px;
-padding-botton: 0px;
-font-size: 20px;`
+padding-bottom: 0px;
+font-size: 30px;`
 
 const Icon2Style = styled.div`
 display: inline-flex;
-text-align: right;
 background-color: white;
-padding: 30px;
+padding-top: 30px;
 padding-left: 30px;
 padding-right: 0px;
-padding-botton: 100px;
-font-size: 20px;`
+padding-bottom: 0px;
+font-size: 30px;`
+
+const Icon1 = styled.div`
+display: inline-flex;
+background-color: #c0c0c0;
+border-radius:50%;
+margin-top: 200px;
+margin-right: 0px;
+margin-bottom: 0px;
+margin-left: 0px;
+width: 100px;
+heights: 100px;`
+
+const Icon2 = styled.div`
+display: inline-flex;
+background-color: #c0c0c0;
+border-radius:50%;
+margin-top: 0px;
+margin-right: 0px;
+margin-bottom: 0px;
+margin-left: 0px;
+width: 100px;
+heights: 100px;`
+
+const Astyle = styled.div`
+display: inline-flex;`
+
 
 function Home() {
   return (
     <div>
       <BodyStyle>GDSC<br />membership</BodyStyle>
       <Link to="/member1">
-       <Icon1Style><img src="src/components/img/one.png" alt="one"></img>メンバー登録</Icon1Style>
+      <Astyle>
+       <Icon1><img src="https://s3-alpha-sig.figma.com/img/90e2/9c9d/5e9868c346f6b006ae4aefdd010a801f?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HqwZzyxvR8y-JHJMYeO4u2fLT0bjqXoC3bvGWvxjAY0QgwXiouekaWyD4e~hpQ-JdlQctE1G5GXSyXvnU0MTrgtXBLMnjpe9QDnDFeQz1iscl5ScIY0DomuV9pQgOR7DDgztw3EHITMiHf6MrMDDinVB-nwR3h27IKT~w~DKexllPZ5KF6ySxSFTNQ~r46M~1PJXwDUJk2K-uySlIPSDiXH1QcD8RMTA0JsjwBxNTbNkqLyye0ZWCRdLnrkvm47EH3xblmpaT9vFlmez52LvIo8d~p9kjgUkOU1VUwgP2~mijYBWDExBV8l~EOOuL05o5Dsr2K0S-evqhJBtha5cQg__" alt="one"></img></Icon1>
+       <Icon1Style>メンバー登録</Icon1Style>
+      </Astyle>
       </Link>
       <TextStyle>説明テキストテキストテキストテキ<br />ストテキストテキストテキストテキ<br />スト</TextStyle>
       <Link to="/member2">
-      <Icon2Style><img src="src/components/img/three.png" alt="three"></img>メンバー一覧</Icon2Style>
+      <Astyle>
+      <Icon2><img src="https://s3-alpha-sig.figma.com/img/d929/eca2/13c09fbfa4cf31835a81a543059784a2?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Z50CrsYyMR5kvC2BG48-vDtKU5TQFdgqNwWMzXkrqw2iYsSc-0MahrzxRfuOFr4S-HdhiO8Vq8juBgrE2k4JvDn51iM8ZLqzxWu67jbyyAD~55x4U38ke2CHoG2qE7Ia9yXbOduUevIVYpIcFb51c57B8FE0fCtk5ZiStMVr-oftyaqVcfyBfj2Ax5Xggt0a-y9puOuyo2z0xvIjWYDpanMAPfWUgzAS4xYreeczc-Nm-IGAqtv0JTBzAaU0yvgwDK3t7W6GYxQAZTXdtqQzQ4KiTmCXPT4~87woUTzqRwRhaR-FQZRymy~sU4cW5aXIomQLMPiSA41gddsOL0Votw__" alt="three"></img></Icon2>
+      <Icon2Style>メンバー一覧</Icon2Style>
+      </Astyle>
       </Link>
     </div>
     );
